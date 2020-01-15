161accd1994d568998c79c9b951cf91549da2b23
Kevin Eaton <kevinhowardeaton@gmail.com>
2020-01-15 15:49:51 -0500

    sum and recursive sum algos

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,10 +3,7 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
-	if list == nil || len(list) == 0 {
-		return false
-	}
-
+	// range will ignore the nil list, so we don't need an explicit check
 	for i := range list {
 		if list[i] == num {
 			return true
--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,17 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	sum := 0
+	for i := range numbers {
+		sum += numbers[i]
+	}
+	return sum
+}
+
+// RecursiveSum implements the summing algo using recursion
+func RecursiveSum(numbers []int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+	return numbers[0] + RecursiveSum(numbers[1:])
 }
