8cbb4ab326e52448c69b197fb0a1ab23ae4fc199
Kevin Eaton <kevinhowardeaton@gmail.com>
2020-01-16 21:03:25 -0500

    added non recursive GCD algo

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,10 +1,18 @@
 package module01
 
-// GCD gets the greatest common denominator using the Euclidean algorithm
-func GCD(a, b int) int {
+// GCDEuclidean gets the greatest common denominator using the Euclidean algorithm recursively
+func GCDEuclidean(a, b int) int {
 	if b == 0 {
 		return a
 	}
 	a, b = b, a%b
-	return GCD(a, b)
+	return GCDEuclidean(a, b)
+}
+
+// GCD gets the greatest common denominator without using recursion
+func GCD(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
