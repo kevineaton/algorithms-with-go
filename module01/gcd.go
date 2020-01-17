24f3e01f262190c4870a784d6191e618c301be88
Kevin Eaton <kevinhowardeaton@gmail.com>
2020-01-16 21:00:58 -0500

    gcd implemented

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,10 @@
 package module01
 
+// GCD gets the greatest common denominator using the Euclidean algorithm
 func GCD(a, b int) int {
-	return 0
+	if b == 0 {
+		return a
+	}
+	a, b = b, a%b
+	return GCD(a, b)
 }
