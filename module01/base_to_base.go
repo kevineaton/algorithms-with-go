2bb9ceb13e80855e8d6a82844eb5e0341ccc293e
Kevin Eaton <kevinhowardeaton@gmail.com>
2020-01-16 19:21:49 -0500

    base to base conversion

--- module01/base_to_base.go
+++ module01/base_to_base.go
@@ -9,5 +9,7 @@ package module01
 //   BaseToBase("E", 16, 2) => "1110"
 //
 func BaseToBase(value string, base, newBase int) string {
-	return ""
+	inDecimal := BaseToDec(value, base)
+	result := DecToBase(inDecimal, newBase)
+	return result
 }
